6a20b2b8781bf53b9910a29268b28652df122450
Bui Dac Huy <huybuidac@gmail.com>
2015-09-06 09:18:43 +0700

    first commit

---
+++ SmsSync/ConstantValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmsSync
+{
+    class ConstantValue
+    {
+        public static int PORT = 26453;
+    }
+}
---
+++ SmsSync/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SmsSync
+{
+    public partial class Form1 : Form
+    {
+        private Thread _threadServer ;
+        private UdpClient _udpPacket;
+        private bool _run;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _run = true;
+            _threadServer = new Thread(CreateUdp);
+
+        }
+
+        private void CreateUdp()
+        {
+            _udpPacket = new UdpClient(ConstantValue.PORT);
+            _udpPacket.EnableBroadcast = true;
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            IPAddress from_addr = null;
+            while (_run)
+            {
+                try
+                {
+                    var receiveBytes = _udpPacket.Receive(ref remoteIpEndPoint);
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    from_addr = remoteIpEndPoint.Address;
+                    MessageBox.Show(returnData + " " + from_addr);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _threadServer.Start();
+        }
+    }
+}
